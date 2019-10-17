26906a950964df3d748c6e99f63ff6e8de1f47c7
Maaz Bin Tahir <diagaaos@gmail.com>
2019-10-18 01:04:20 +0500

    Enabled auth token app

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.User)
+admin.site.register(auth.Token)
 admin.site.register(models.Board)
 admin.site.register(models.Task)
 admin.site.register(models.Group)
